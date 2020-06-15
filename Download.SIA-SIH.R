e4403a14ad6b594c03cc81c7b14b530f1f9e45a8
Patrick Dias <65247906+diaspv@users.noreply.github.com>
2020-06-15 16:13:35 -0300

    Update Download.SIA-SIH.R

--- Download.SIA-SIH.R
+++ Download.SIA-SIH.R
@@ -1,12 +1,15 @@
-#Download de arquivos SIA/SUS e SIH/SUS
-#Patrick Dias - github.com/diaspv
+# Download de arquivos públicos do SUS: atendimentos ambulatoriais (SIA/SUS) e
+# internações hospitalares (SIH/SUS)
+# Patrick Dias - github.com/diaspv
 
 #Para executar este script no RGui: Arquivo >> Interpretar código fonte R... >> 
-#Para executar este script no RStudio: <Ctrl><Shift><Enter>
+#Para executar este script no RStudio: <Ctrl> + <Shift> + <Enter>
 
 
 if (!require("RCurl")) install.packages("RCurl")
-require("RCurl")
+if (!require("stringr")) install.packages("stringr")
+library(RCurl)
+library(stringr)
 
 # Identificar as variáveis pré existentes no ambiente
 var_exist <- ls()
@@ -22,7 +25,7 @@ SIH_DB <- c("RD", "SP", "RJ", "ER")
 SIA_DB <- c("AB", "ABO", "ACF", "AD", "AM", "AN", "AQ", "AR", "ATD", "PA", "PS", "SAD")
 DB <- select.list(eval(parse(text = paste0(SIS,"_DB"))), title="Selecione o Banco de Dados", graphics = TRUE)
 UF <- select.list(c("AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"), title="Selecione o(s) estado(s):", graphics = TRUE, multiple=TRUE)
-AA <- select.list(stringr::str_pad(seq(8, format(Sys.Date(), "%y")), 2, pad = "0"), title="Selecione o(s) ano(s):", graphics = TRUE, multiple=TRUE)
+AA <- select.list(str_pad(seq(8, format(Sys.Date(), "%y")), 2, pad = "0"), title="Selecione o(s) ano(s):", graphics = TRUE, multiple=TRUE)
 setwd(choose.dir(caption = "Selecione a pasta de destino"))
 
 # Identifica todos os arquivos disponíveis para download no diretório escolhido
